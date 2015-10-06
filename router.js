36aeca4bf7de63f98fa570beca4bea65b98452c8
Hannecke <marhanes21@gmail.com>
2015-10-06 09:52:23 +0100

    Create router.js

---
+++ router.js
@@ -0,0 +1,68 @@
+var Profile = require("./weather.js");
+var views = require("./views.js");
+var queryString = require("querystring");
+
+var commonHeaders =  {'Content-Type': 'text/html'};
+// HANDLE THE HTTP route GET /and POST i.e. Home
+function home(request,response){
+  //if url =="/" && GET
+
+  if(request.url === '/'){
+    if(request.method.toLowerCase() === "get"){ // al cargar la pagina usamos metodo get 
+    
+      response.writeHead(200, commonHeaders);
+      views.view("index", {}, response);
+      response.end();
+    
+    }else{ // al request el form usamos metodo get 
+    
+    //if url == "/" && POST
+    //get post
+    request.on("data", function(postBody){
+       console.log(postBody.toString());
+       var query = queryString.parse(postBody.toString()); //pasamos postBody de string a un object en query con querystring.parse
+       response.writeHead(303, {Location:"/" + query.cityName}); // 303 para cambiar de location y {Location: url}
+       response.end();
+    //redirect to /:username
+    });   
+  }  
+ }
+}
+// handle http route GET /:USERNAME i.e. /marcoshanneckesteve
+function user(request, response){
+ 
+  var username = request.url.replace("/", "");
+  if(username.length > 0){
+      response.writeHead(200, commonHeaders);
+      //get JSON from weather API
+      var studentProfile = new Profile(username);
+      
+      studentProfile.on("end", function(profileJSON){       
+        var values = {
+          temperature: (profileJSON.main.temp - 273.15).toFixed(2),
+          sky: profileJSON.weather[0].main,
+          name: profileJSON.name
+
+        }
+        //show profile
+          //response.write(values.temperature+ " " + values.sky + ' '+ values.name);
+          views.view("index",values, response);
+          views.view("footer",values, response);      
+          response.end('');
+
+     }); 
+      studentProfile.on("error", function(error){
+      
+        response.end('Error\n'); 
+      //on "error"
+        //show the error
+      }); 
+
+  }
+}
+
+
+
+
+module.exports.home = home;
+module.exports.user = user;
