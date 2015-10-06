a609021dfa7688d6e307da9ec69179ebb7cdfdd4
Hannecke <marhanes21@gmail.com>
2015-10-06 09:51:17 +0100

    Create app.js

---
+++ app.js
@@ -0,0 +1,14 @@
+//app.js
+var router= require("./router.js");
+
+//1.CREATE A WEB SERVER
+
+var http = require('http');
+
+http.createServer(function (request, response) {
+  router.home(request, response);
+  router.user(request,response);
+   
+}).listen(3000);
+
+console.log('Server running at http://<workspace_url>/');
